0bf4898fb1c563f709a34bd4e18612ebc08dddc3
Jayandhan <jayandhanrajendran03@gmail.com>
2023-11-08 22:39:52 +0530

    vanta

--- src/Author.js
+++ src/Author.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
+import GLOBE from 'vanta/src/vanta.globe';
+import { useEffect } from 'react';
+import './App.css';
 
 const Author = () => {
-  return (
+
+    useEffect(() => {
+      GLOBE({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        size: 0.60
+      })
+    })
+    return (
     <Card
       sx={{
         width: '78%',
@@ -18,15 +35,19 @@ const Author = () => {
       }}
       className="file-share-card"
     >
-      <div className="author" style={{ textAlign: 'center' }}>
+      {/* <div className='bg' > */}
+      <div className="author"  style={{ textAlign: 'center' }}>
+      
         <h3 style={{ fontFamily: 'Arial, sans-serif', fontSize: '24px', fontWeight: 'bold' }}>
           404 Error
         </h3>
+        <div className='bg' id='vanta'></div>
         <p className="bio-text">Page not found.</p>
         <Button variant="outlined" href="/" color="primary">
           Go to Homepage
         </Button>
-      </div>
+        </div>
+      {/* </div> */}
     </Card>
   );
 };
