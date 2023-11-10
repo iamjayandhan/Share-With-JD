e54e13b76b0060ca9551dab7bb3b119ab81a249c
Jayandhan <jayandhanrajendran03@gmail.com>
2023-11-10 11:31:07 +0530

    Timestamp

--- src/TextShare.js
+++ src/TextShare.js
@@ -7,6 +7,7 @@ import {
   addDoc,
   doc,
   deleteDoc,
+  serverTimestamp,
 } from "firebase/firestore";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -17,6 +18,8 @@ import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert'; // Import the Alert component
 import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 
 function TextShare() {
   const [sharedText, setSharedText] = useState("");
@@ -25,17 +28,42 @@ function TextShare() {
   const [showEmptyTextAlert, setShowEmptyTextAlert] = useState(false); // State for the empty text alert
   const [showShareSuccessAlert, setShowShareSuccessAlert] = useState(false); // State for the share success alert
   const [searchText, setSearchText] = useState("");
+  const [hoveredMessage, setHoveredMessage] = useState(null);
+
+  const handleHover = (id) => {
+    setHoveredMessage(id);
+  };
+
+  const handleUnhover = () => {
+    setHoveredMessage(null);
+  };
+
+  const formatTimestamp = (timestamp) => {
+    if (timestamp && timestamp.toDate) {
+      const jsDate = timestamp.toDate(); // Convert Firestore Timestamp to JavaScript Date
+      return jsDate.toLocaleString(); // Format the date as a string
+    }
+    return "{Fetching...}"; // Return an empty string if timestamp is undefined or does not have toDate function
+  };
 
   const shareText = async () => {
     if (sharedText.trim() !== "") {
       try {
-        const newDocRef = await addDoc(sharedCollectionRef, { text: sharedText });
-        setSharedData([...sharedData, { text: sharedText, id: newDocRef.id }]);
-        setSharedText("");
-        setShowEmptyTextAlert(false); // Hide the empty text alert
-        setShowShareSuccessAlert(true); // Show the share success alert
+        const timestamp = serverTimestamp(); // Use serverTimestamp() to get the current server timestamp
+        const newDocRef = await addDoc(sharedCollectionRef, {
+          text: sharedText,
+          timestamp: timestamp,
+        });
 
-        // Hide the share success alert after a few seconds (e.g., 5 seconds)
+        setSharedData([
+          ...sharedData,
+          { text: sharedText, timestamp: timestamp, id: newDocRef.id },
+        ]);
+
+        setSharedText("");
+        setShowEmptyTextAlert(false);
+        setShowShareSuccessAlert(true);
+
         setTimeout(() => {
           setShowShareSuccessAlert(false);
         }, 5000);
@@ -43,29 +71,28 @@ function TextShare() {
         console.error("Error sharing text:", error);
       }
     } else {
-      setShowEmptyTextAlert(true); // Show the empty text alert
+      setShowEmptyTextAlert(true);
 
-      // Hide the empty text alert after a few seconds (e.g., 5 seconds)
       setTimeout(() => {
         setShowEmptyTextAlert(false);
       }, 5000);
     }
-  }
+  };
 
   const getSharedData = async () => {
     const data = await getDocs(sharedCollectionRef);
-    setSharedData(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-  }
+    setSharedData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
 
   const deleteSharedData = async (id) => {
     try {
       const sharedDoc = doc(db, "textshare", id);
       await deleteDoc(sharedDoc);
-      setSharedData(sharedData.filter(data => data.id !== id));
+      setSharedData(sharedData.filter((data) => data.id !== id));
     } catch (error) {
       console.error("Error deleting shared data:", error);
     }
-  }
+  };
 
   const copyText = (text) => {
     navigator.clipboard.writeText(text)
@@ -75,13 +102,13 @@ function TextShare() {
       .catch((error) => {
         console.error("Copy to clipboard failed:", error);
       });
-  }
+  };
 
   useEffect(() => {
     getSharedData();
   }, []);
 
-  const filteredSharedData = sharedData.filter(data =>
+  const filteredSharedData = sharedData.filter((data) =>
     data.text.toLowerCase().includes(searchText.toLowerCase())
   );
 
@@ -147,25 +174,77 @@ function TextShare() {
           }}
         />
         <ul style={{ padding: 0 }}>
-          {filteredSharedData.map(data => (
-            <Card sx={{
-              width: "90%",
-              maxWidth: 300,
-              marginTop: 2,
-              boxShadow: 3,
-              marginBottom: 3,
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-            }} className="shared-card" key={data.id}>
-              <li style={{ margin: 0, padding: "15px 0px", paddingLeft: "10px", paddingRight: "10px", overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap'}}>
+          {filteredSharedData.map((data) => (
+            <Card
+              key={data.id}
+              sx={{
+                width: "90%",
+                maxWidth: 300,
+                marginTop: 2,
+                boxShadow: 3,
+                marginBottom: 3,
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                position: "relative",
+              }}
+              className="shared-card"
+            >
+              <li
+                style={{
+                  margin: 0,
+                  padding: "15px 0px",
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  whiteSpace: "nowrap",
+                }}
+              >
                 {data.text}
                 <CardActions className="full-card">
                   <div className="button-container">
-                    <Button onClick={() => copyText(data.text)} variant="contained" size="medium" sx={{ marginRight: 1 }}>Copy</Button>
-                    <Button variant="contained" onClick={() => deleteSharedData(data.id)} size="medium" startIcon={<DeleteIcon />}>Delete</Button>
+                    <Button
+                      onClick={() => copyText(data.text)}
+                      variant="contained"
+                      size="medium"
+                      sx={{ marginRight: 1 }}
+                    >
+                      Copy
+                    </Button>
+                    <Button
+                      variant="contained"
+                      onClick={() => deleteSharedData(data.id)}
+                      size="medium"
+                      startIcon={<DeleteIcon />}
+                    >
+                      Delete
+                    </Button>
+                    <IconButton
+                      onMouseEnter={() => handleHover(data.id)}
+                      onMouseLeave={handleUnhover}
+                    >
+                      <InfoIcon />
+                    </IconButton>
                   </div>
                 </CardActions>
+                {data.timestamp && (
+                  <Typography
+                    variant="caption"
+                    color="textSecondary"
+                    style={{
+                      position: "absolute",
+                      bottom: "5px",
+                      right: "5px",
+                      display: hoveredMessage === data.id ? "block" : "none",
+                      background: "#fff",
+                      padding: "5px",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    Shared on {formatTimestamp(data.timestamp)}
+                  </Typography>
+                )}
               </li>
             </Card>
           ))}
